251e81aa0eb28e30f31c18cb45e26a8025741342
麦仁丝 <1211012802@qq.com>
2026-05-06 19:59:30 +0800

    2026年5月6日19:59:28

--- website/__main__.py
+++ website/__main__.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/event')
+def event():
+    return render_template("event.html")
+
+@app.route('/competition')
+def competition():
+    return render_template("competition.html")
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
