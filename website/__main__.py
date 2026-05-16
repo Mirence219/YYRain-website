73c79e649700baae6886ec9d612f7aef1d3d2b55
麦仁丝 <1211012802@qq.com>
2026-05-17 00:33:36 +0800

    设置了灰色纯色背景以及调试模式， 以还原设置被色背景前的效果，同时避免深色系统浏览器由于最底层背景为纯黑导致的显示偏暗的问题 新增了调试模式，方便调试时可以在其他设备通过局域网访问网站以供测试。

--- website/__main__.py
+++ website/__main__.py
@@ -1,5 +1,6 @@
 from doctest import debug
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -19,7 +20,18 @@ def competition():
 def wiki():
     return render_template("wiki.html")
 
+DEBUG_MODE = os.getenv("Debug", "false") == "true"
+
+
+
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080)
+    print(DEBUG_MODE)
+    if DEBUG_MODE:
+        print("【当前处于调试状态】")
+        host = "0.0.0.0"
+    else:
+        print("【当前处于实际运行】")
+        host = "127.0.0.1"
+    app.run(host=host, port=8080)
